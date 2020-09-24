19746813181fe20505a71472759fc2c2f6bedd4f
Shujaat Azim <shujaat.azim@gmail.com>
2020-09-24 19:44:10 -0400

    Built out App component in order to test fecth calls to backend. Rendering data successfully.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  let [users, setUsers] = useState([])
+  let [games, setGames] = useState([])
+  let [achievements, setAchievements] = useState([])
+  let [displayed, setDisplayed] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+    .then(resp => resp.json())
+    .then(data => setUsers(data))
+    
+    fetch('http://localhost:3000/games')
+    .then(resp => resp.json())
+    .then(data => setGames(data))
+
+    fetch('http://localhost:3000/achievements')
+    .then(resp => resp.json())
+    .then(data => setAchievements(data))
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setDisplayed("users")}>Users</button>
+      <button onClick={() => setDisplayed("games")}>Games</button>
+      <button onClick={() => setDisplayed("achievements")}>Achievements</button>
+
+      <ol>
+      { displayed === "users" ? users.map(user => <li key={user.name}>{user.name}</li>) :
+        displayed === "games" ? games.map(game => <li key={game.name}>{game.name}</li>) :
+        displayed === "achievements" ? achievements.map(achievement => <li key={achievement.title}>{achievement.title}</li>) :
+        null
+      }
+      </ol>
     </div>
   );
 }
